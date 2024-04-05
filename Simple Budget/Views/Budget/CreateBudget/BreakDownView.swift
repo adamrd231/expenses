e7b0f788dde00f907f8ace5ca43bf63b1302b40d
Adam Reed <adam.rdconcepts@gmail.com>
2024-04-05 00:00:35 -0400

    Clean up sliders

--- Simple Budget/Views/Budget/CreateBudget/BreakDownView.swift
+++ Simple Budget/Views/Budget/CreateBudget/BreakDownView.swift
@@ -29,9 +29,9 @@ struct BreakDownView: View {
                 }
             }
             .font(.caption)
-            BreakdownSliderView(value: $newBudget.needsBudgetPercentage, type: .needs)
-            BreakdownSliderView(value: $newBudget.wantsBudgetPercentage, type: .wants)
-            BreakdownSliderView(value: $newBudget.saveBudgetPercentage, type: .save)
+            SliderView(value: $newBudget.needsBudgetPercentage, type: .needs)
+            SliderView(value: $newBudget.wantsBudgetPercentage, type: .wants)
+            SliderView(value: $newBudget.saveBudgetPercentage, type: .save)
             Spacer()
         }
         .padding()
@@ -44,45 +44,3 @@ struct BreakDownView_Previews: PreviewProvider {
     }
 }
 
-extension View {
-    func border(width: CGFloat, edges: [Edge], color: Color) -> some View {
-        overlay(EdgeBorder(width: width, edges: edges).foregroundColor(color))
-    }
-}
-
-struct EdgeBorder: Shape {
-    var width: CGFloat
-    var edges: [Edge]
-
-    func path(in rect: CGRect) -> Path {
-        edges.map { edge -> Path in
-            switch edge {
-            case .top: return Path(.init(x: rect.minX, y: rect.minY, width: rect.width, height: width))
-            case .bottom: return Path(.init(x: rect.minX, y: rect.maxY - width, width: rect.width, height: width))
-            case .leading: return Path(.init(x: rect.minX, y: rect.minY, width: width, height: rect.height))
-            case .trailing: return Path(.init(x: rect.maxX - width, y: rect.minY, width: width, height: rect.height))
-            }
-        }.reduce(into: Path()) { $0.addPath($1) }
-    }
-}
-
-struct BreakdownSliderView: View {
-    @Binding var value: Double
-    let type: BudgetCategory
-    var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                switch type {
-                case .needs: Text("Needs")
-                case .wants: Text("Wants")
-                case .save: Text("Save")
-                case .income: Text("Income")
-                }
-                
-                Text(value, format: .percent.precision(.fractionLength(0)))
-            }
-            Slider(value: $value, in: 0...1)
-        }
-        .padding()
-    }
-}
---
+++ Simple Budget/Views/Budget/CreateBudget/Components/SliderView.swift
@@ -0,0 +1,39 @@
+//
+//  SliderView.swift
+//  Simple Budget
+//
+//  Created by Adam Reed on 4/4/24.
+//
+
+import SwiftUI
+
+struct SliderView: View {
+    @Binding var value: Double
+    let type: BudgetCategory
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                switch type {
+                case .needs: Text("Needs")
+                case .wants: Text("Wants")
+                case .save: Text("Save")
+                case .income: Text("Income")
+                }
+                Text(value,  format: .percent.precision(.fractionLength(0)))
+            }
+            .bold()
+            HStack {
+                Text(0, format: .percent)
+                Slider(value: $value, in: 0...1)
+                Text(100, format: .percent)
+            }
+        }
+    }
+}
+
+
+struct SliderView_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderView(value: .constant(0.1), type: .income)
+    }
+}
