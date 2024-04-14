9c0761b256573772a647b516387d7a4d3dc0c5b8
Adam Reed <adam.rdconcepts@gmail.com>
2024-04-14 18:51:39 -0400

    Update individual transaction view

--- Simple Budget/Views/Transactions/IndividualTransactionView.swift
+++ Simple Budget/Views/Transactions/IndividualTransactionView.swift
@@ -4,14 +4,45 @@ struct IndividualTransactionView: View {
     @State var transaction: Transaction
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text(transaction.name)
+        VStack(alignment: .leading, spacing: 10) {
+            HStack {
+                Text("Date").bold()
+                Spacer()
+                Text(transaction.date, format: .dateTime)
+            }
+            HStack {
+                Text("Amount").bold()
+                Spacer()
+                Text(transaction.amount, format: .currency(code: "USD"))
+            }
+            HStack {
+                Text("Category").bold()
+                Spacer()
+                Text(transaction.type.name)
+            }
+            HStack {
+                Text("Type").bold()
+                Spacer()
+                Text(transaction.category.description)
+            }
+            VStack(alignment: .leading) {
+                Text("Details").bold()
+                Text(transaction.description)
+            }
+            .padding(.top)
+            
+            Spacer()
         }
+        .padding()
+        .navigationTitle(transaction.name)
     }
 }
 
 struct IndividualTransactionView_Previews: PreviewProvider {
     static var previews: some View {
-        IndividualTransactionView(transaction: dev.transaction)
+        NavigationStack {
+            IndividualTransactionView(transaction: dev.transaction)
+        }
+        .navigationTitle("Transaction")
     }
 }
